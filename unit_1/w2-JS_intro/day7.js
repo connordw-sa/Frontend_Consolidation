0ab501ff42df64465e3f2895adc7425d01861ad6
Connor Wehmeyer <114667779+connordw-sa@users.noreply.github.com>
2023-09-26 22:03:02 +0000

    Update day7.js

--- unit_1/w2-JS_intro/day7.js
+++ unit_1/w2-JS_intro/day7.js
@@ -21,53 +21,69 @@
  -----------------------------------------------------------------------
 */
 
+function randomInteger(value) {
+  let randomNumber = Math.floor(Math.random() * value);
+  return randomNumber;
+}
+
 /* EXERCISE 3
  Write the code to execute an addition (a sum) of the numbers 12 and 20.
 */
-
-let ex3Answer = 12 + 20;
+console.log(
+  "\nExercise 3-----------------------------------------------------\n" +
+    (12 + 20)
+);
 
 /* EXERCISE 4
  Create a variable named x and assign to it the number 12.
 */
 
 let x = 12;
+console.log(
+  "\nExercise 4-----------------------------------------------------\n" + x
+);
 
 /* EXERCISE 5
  Create a variable called name and assign to it the string "John Doe".
 */
 
 let name = "John Doe";
+console.log(
+  "\nExercise 5-----------------------------------------------------\n" + name
+);
 
 /* EXERCISE 6
  Execute a subtraction between the number 12 and the variable x you declared before (which is storing the value 12).
 */
-
-let ex6Answer = x - 12;
+console.log(
+  "\nExercise 6-----------------------------------------------------\n" +
+    (x - 12)
+);
 
 /* EXERCISE 7
  Create two variables: name1 and name2. Assign to name1 the string "john", and to name2 the string "John" (this one has a capital J!)
  Verify that name1 is different from name2 (HINT: check their equality to be false).
  Finally, verify that the equality between name1 and name2 becomes true if both are lowercase (without changing the value of name2).
 */
-
-let name1 = "john";
-let name2 = "John";
-const isEqual = name1 === name2;
-const message = isEqual ? "They are equal" : "They are not equal";
-console.log(message);
-if (name1.toLowerCase() === name2.toLowerCase()) {
-  console.log("They are equal");
-}
+console.log(
+  "\nExercise 7-----------------------------------------------------"
+);
+console.log(
+  "john" === "John"
+    ? "They are equal"
+    : "They are not equal" + "john" === "John".toLowerCase()
+    ? "They're equal"
+    : "Not equal"
+);
 
 /* EXERCISE 8
  Create a variable named x, and assign to it a numeric value less than 10.
  Write the code to print the literal value of x (ex.: if x is 1, it should print "one", if x is 5 it should print "five" etc.).
 */
-
-let x2 = Math.floor(Math.random() * 12);
-
-switch (x2) {
+console.log(
+  "\nExercise 8-----------------------------------------------------"
+);
+switch (randomInteger(11)) {
   case 1:
     console.log("one");
     break;
@@ -84,7 +100,7 @@ switch (x2) {
     console.log("five");
     break;
   case 6:
-    console.log("six");
+    "six";
     break;
   case 7:
     console.log("seven");
@@ -103,10 +119,10 @@ switch (x2) {
  [Extra] Assign a value to a variable using a ternary operator (this topic has not been covered during lesson yet,
  try to search it by yourself, tomorrow morning we'll discuss it together!)
 */
-
-const ex9Number = Math.floor(Math.random() * 15);
-const greaterThan = ex9Number > 10 ? "Its bigger than 10" : "Its less than 10";
-console.log(greaterThan);
+console.log(
+  "\nExercise 9-----------------------------------------------------"
+);
+console.log(randomInteger(15) > 10 ? "Its bigger than 10" : "Its less than 10");
 
 // Additional assignments for Day 3
 
@@ -114,22 +130,36 @@ console.log(greaterThan);
  Use a ternary operator to assign to a variable called gender the string values "male" or "female".
  The choice should be made based on the value of another variable called isMale.
 */
-
-let isMale = "male";
-let gender = isMale == "male" ? "They are male" : "They are female";
-console.log(gender);
+console.log(
+  "\nExercise 1 Extra-----------------------------------------------------"
+);
+console.log(+Math.random() < 0.5 ? "They are male" : "They are female");
 
 /*EX 2
  Write a piece of code for checking if, given two integers, the value of one of them is 8 or if their addition or subtraction is equal to 8.
 */
+console.log(
+  "\nExercise 2 Extra-----------------------------------------------------"
+);
+let numero1 = randomInteger(20);
+let numero2 = randomInteger(20);
 
-/* WRITE YOUR ANSWER HERE */
+if (
+  numero1 == 8 ||
+  numero2 == 8 ||
+  numero1 + numero2 == 8 ||
+  Math.abs(numero1 - numero2) == 8
+) {
+  console.log("One of them is 8 or they add/subtract to 8");
+}
 
 /*EX 3
  Create a variable and assign to it the concatenation of two strings.
 */
-
-/* WRITE YOUR ANSWER HERE */
+console.log(
+  "\nExercise 3 Extra-----------------------------------------------------\n" +
+    "Hi".concat(" There")
+);
 
 /*EX 4
  Create three variables and assign a numerical value to each one of them. 
@@ -137,7 +167,15 @@ console.log(gender);
  Display the result in the console.
 */
 
-/* WRITE YOUR ANSWER HERE */
+let varArray = [randomInteger(10), randomInteger(10), randomInteger(10)].sort(
+  function (a, b) {
+    return b - a;
+  }
+);
+console.log(
+  "\nExercise 4 Extra-----------------------------------------------------\n" +
+    varArray
+);
 
 /*EX 5
  Write a piece of code for finding the average of two given integers.
